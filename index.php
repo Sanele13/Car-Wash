ca8645554c9e3bb92c6f1bb35984c768ba59e65b
Sanele13 <aynstyn@gmail.com>
2018-01-12 06:53:02 +0200

    Added styling to version control

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Q's Car Wash</title>
+	<link rel="stylesheet" type="text/css" href="http://127.0.0.1/car%20wash/css/mystyle.css">
 </head>
 <body>
-
+<?php include("templates/header.php"); ?>
+Some text on the body.
 </body>
-</html>>
+</html>
---
+++ templates/header.php
@@ -0,0 +1,14 @@
+<div class = "header">
+	<div class = "logo">
+		<!--img src = ""/-->
+	</div>
+	<div class = "login">
+		<form action="login.php">
+			
+			<span style="color: white;font-weight: bold;">Username</span>
+			<input type="text" name="username"/><br/>
+			<span style="color: white;font-weight: bold;">Password</span>
+			<input type="password" name="password"/><br/><br/>
+		</form>
+	</div>
+</div>
