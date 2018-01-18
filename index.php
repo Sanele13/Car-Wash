832d6e52b97864a6231ae31c786cc724d2dbf6b4
Sanele13 <aynstyn@gmail.com>
2018-01-18 11:48:44 +0200

    I have added the navigation menu. I'm gonna style it and then move on to the home page banner.

--- index.php
+++ index.php
@@ -5,7 +5,10 @@
 	<link rel="stylesheet" type="text/css" href="http://127.0.0.1/car%20wash/css/mystyle.css">
 </head>
 <body>
-<?php include("templates/header.php"); ?>
+	<?php 
+		include("templates/header.php");
+		include("templates/menu.php"); 
+	?>
 Some text on the body.
 </body>
 </html>
---
+++ templates/menu.php
@@ -0,0 +1,13 @@
+<div class = "nav-menu-container">
+	<ul class="nav-menu">
+		<li class="nav-menu-item">
+			<span >Home</span>
+		</li>
+		<li class="nav-menu-item">
+			<span>Services</span>
+		</li>
+		<li class="nav-menu-item">
+			<span>About</span>
+		</li>
+	</ul>
+</div>
