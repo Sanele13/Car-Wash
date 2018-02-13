34848511cd5d3df62f540f4a2fa3c0d9126b976b
Sanele13 <aynstyn@gmail.com>
2018-02-13 22:22:11 +0200

    Added required data variables

--- register/index.php
+++ register/index.php
@@ -1,4 +1,11 @@
 <?php
 	//name | last_name | password | profile_pic | email | number
-	
+
+	//get data
+	$name = $_POST['name'];
+	$last_name = $_POST['last_name'];
+	$password = $_POST['password'];
+	$profile_pic = $_POST['profile_pic'];
+	$email = $_POST['email'];
+	$number = $_POST['number'];
 ?>
