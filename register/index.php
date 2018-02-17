0d2d60e5223f233dd7e57e149ebcd8ae8be305bb
Sanele13 <aynstyn@gmail.com>
2018-02-17 10:23:47 +0200

    This is bad practice!! I am commiting changes I made to the site. I forgot what I added.

--- register/index.php
+++ register/index.php
@@ -5,7 +5,20 @@
 	$name = $_POST['name'];
 	$last_name = $_POST['last_name'];
 	$password = $_POST['password'];
-	$profile_pic = $_POST['profile_pic'];
+	$profile_pic = $_FILES['profile_pic']['name'];
 	$email = $_POST['email'];
 	$number = $_POST['number'];
+	echo '../images/profile/'.$name.'/'.$profile_pic;
+	mkdir('../images/profile/'.$name.'/');
+	//save image
+	if( $_FILES['profile_pic']['name'] != "" ){
+		copy( $_FILES['profile_pic']['tmp_name'], '../images/profile/'.$name.'/'.$profile_pic ) or die( "Could not copy file!");
+	}
+	//store data
+	$conn = mysqli_connect("localhost","root","zukiswa","car_wash"); //connect to server and select db
+	//$statement = "insert into user_details(name,last_name,password,profile_pic,email,number) values ('{$name}','{last_name}','{$password}','{profile_pic}','{email}','{number}')";//query
+	//$query = mysqli_query($conn, $statement);
+
+	//$conn->mysql_close();
+
 ?>
---
+++ register/reg.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Create Account</title>
+		<link rel="stylesheet" type="text/css" href="http://127.0.0.1/car%20wash/css/mystyle.css">
+	</head>
+<body>
+	<?php 
+		//session_start();
+		include("../templates/header.php");
+		include('../templates/menu.php');
+	 ?>
+	<div class = "reg">
+		<form action="index.php" method = "POST" enctype="multipart/form-data">
+			<input name = "name" type = "text" placeholder = "Name" class="reg_input" /><br/><br/>
+			<input name = "last_name" type = "text" placeholder = "Last name" class="reg_input" /><br/><br/>
+			<input name = "number" type = "text" placeholder = "Cell Number" class="reg_input" /><br/><br/>
+			Select profile image: <input name = "profile_pic" type = "file" /><br/><br/>
+			<input name = "email" type = "text" placeholder = "Email Address" class="reg_input" /><br/><br/>
+			<input name = "password" type = "password" placeholder = "Password" class="reg_input" /><br/><br/>
+			<input name = "conf-password" type = "password" placeholder = "Confirm Password" class="reg_input" /><br/><br/><br/>
+			<input id = "reg_button" name = "submit" type = "submit" value="Sign Up">
+		</form><br/>
+			
+	</div>
+</body>
+</html>
