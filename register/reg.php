d2041e110d8d87e0ef239f2b68a472a0b084553f
Sanele13 <aynstyn@gmail.com>
2018-02-17 11:12:53 +0200

    I have removed the login form on reg page

--- register/reg.php
+++ register/reg.php
@@ -3,6 +3,11 @@
 	<head>
 		<title>Create Account</title>
 		<link rel="stylesheet" type="text/css" href="http://127.0.0.1/car%20wash/css/mystyle.css">
+		<style type="text/css">
+			.login{
+				display: none;
+			}
+		</style>
 	</head>
 <body>
 	<?php 
@@ -15,10 +20,10 @@
 			<input name = "name" type = "text" placeholder = "Name" class="reg_input" /><br/><br/>
 			<input name = "last_name" type = "text" placeholder = "Last name" class="reg_input" /><br/><br/>
 			<input name = "number" type = "text" placeholder = "Cell Number" class="reg_input" /><br/><br/>
-			Select profile image: <input name = "profile_pic" type = "file" /><br/><br/>
 			<input name = "email" type = "text" placeholder = "Email Address" class="reg_input" /><br/><br/>
 			<input name = "password" type = "password" placeholder = "Password" class="reg_input" /><br/><br/>
-			<input name = "conf-password" type = "password" placeholder = "Confirm Password" class="reg_input" /><br/><br/><br/>
+			<input name = "conf-password" type = "password" placeholder = "Confirm Password" class="reg_input" /><br/><br/>
+			Select profile image: <input name = "profile_pic" type = "file" /><br/><br/><br/>
 			<input id = "reg_button" name = "submit" type = "submit" value="Sign Up">
 		</form><br/>
 			
