9f30a4df9bb2ea2131601f7d333e65da9326f277
Sanele13 <aynstyn@gmail.com>
2018-02-18 06:39:30 +0200

    Changed DB credentials

--- register/index.php
+++ register/index.php
@@ -15,7 +15,7 @@
 		copy( $_FILES['profile_pic']['tmp_name'], '../images/profile/'.$name.'/'.$profile_pic ) or die( "Could not copy file!");
 	}
 	//store data
-	$conn = mysqli_connect("localhost","root","zukiswa","car_wash"); //connect to server and select db
+	$conn = mysqli_connect("ec2-23-21-101-174.compute-1.amazonaws.com","rxtgqfxfcogpjk","d5644497df9db5ba0662be23d640c1dab93d7bfb4aad01cc23c3061bcb4ba2fa","d338a782pecr9b"); //connect to server and select db
 	$statement = "insert into user_details(name,last_name,password,profile_pic,email,number) values ('{$name}','{$last_name}','{$password}','{$profile_pic}','{$email}','{$number}')";//query
 	$query = mysqli_query($conn, $statement);
 
